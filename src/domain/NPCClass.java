ec552d88f2882365e8d233995aae493bbc90d671
Alexis Kees Bahl <alexiskeesbahl@gmail.com>
2025-10-11 14:56:25 -0300

    First commit.

--- src/domain/NPCClass.java
+++ src/domain/NPCClass.java
@@ -1,4 +1,14 @@
 package domain;
 
 public class NPCClass {
+    private String name;
+    private String race;
+    private String gender;
+    private String age;
+    private String category;
+    private String job;
+    private String appearence;
+    private String personality;
+    private String quirk;
+
 }
